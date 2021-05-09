ca0d1cb67fc1d28e1ef4a5ee367b96fd9a065d69
Snopok Stanislav <snopok.stanislav@mail.ru>
2021-05-09 20:01:40 +0300

    docs: update readme

--- checkArgs.js
+++ checkArgs.js
@@ -2,22 +2,22 @@ import fs from 'fs'
 
 const checkArgs = (options) => {
     if (options.action !== 'encode' && options.action !== 'decode') {
-        process.stderr.write('Action type is invalid!')
+        process.stderr.write('error: Action type is invalid!')
         process.exit(-1)
     }
 
     if (!options.shift || options.shift % 1 !== 0) {
-        process.stderr.write('Have no shift, or shift is not integer number!')
+        process.stderr.write('error: Have no shift, or shift is not integer number!')
         process.exit(-1)
     }
 
     if (options.input && !fs.existsSync(options.input)) {
-        process.stderr.write(`The input file has been passed, but it can't be read`)
+        process.stderr.write(`error: The input file has been passed, but it can't be read`)
         process.exit(-1)
     }
 
     if (options.output && !fs.existsSync(options.output)) {
-        process.stderr.write(`The output file has been passed, but it can't be written`)
+        process.stderr.write(`error: The output file has been passed, but it can't be written`)
         process.exit(-1)
     }
 }
